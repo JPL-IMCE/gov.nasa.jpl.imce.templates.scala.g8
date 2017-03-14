d0d280450941612efd414d7ec925e67927522392
Sebastian Herzig <Sebastian.J.Herzig@jpl.nasa.gov>
2017-03-13 18:40:11 -0700

    Bugfix

--- src/main/g8/scripts/travis-build.sh
+++ src/main/g8/scripts/travis-build.sh
@@ -3,14 +3,14 @@
 set -ev
 
 # Build if TRAVIS_TAG is unset or empty.
-[ -n "${TRAVIS_TAG}" ] && exit 0;
+[ -n "\${TRAVIS_TAG}" ] && exit 0;
 
 # Get the tag for this commit
-t=$(git name-rev --tags --name-only $(git rev-parse HEAD))
+t=\$(git name-rev --tags --name-only \$(git rev-parse HEAD))
 
 # Bypass the build if the tag is anything but 'undefined'.
-[ "undefined" != "$t" ] && exit 0;
+[ "undefined" != "\$t" ] && exit 0;
 
-. $(dirname $0)/travis-decode.sh
+. \$(dirname \$0)/travis-decode.sh
 
 sbt -jvm-opts travis/jvmopts.compile compile
--- src/main/g8/scripts/travis-deploy.sh
+++ src/main/g8/scripts/travis-deploy.sh
@@ -2,13 +2,13 @@
 
 set -ev
 
-. $(dirname $0)/travis-decode.sh
+. \$(dirname \$0)/travis-decode.sh
 
 # Deploy if TRAVIS_TAG is set.
 # Error if TRAVIS_SECURE_ENV_VARS is false
 
-[ -z "${TRAVIS_TAG}" ] && exit 0;
-[ ! ${TRAVIS_SECURE_ENV_VARS} ] && exit -1;
+[ -z "\${TRAVIS_TAG}" ] && exit 0;
+[ ! \${TRAVIS_SECURE_ENV_VARS} ] && exit -1;
 
 chmod 600 local.*
 eval `ssh-agent -s`
@@ -17,5 +17,5 @@ git config --global push.default simple
 git config --global user.email "nicolas.f.rouquette@jpl.nasa.gov"
 git config --global user.name "Travis CI (on behalf of Nicolas F. Rouquette)"
 
-sbt -jvm-opts travis/jvmopts.compile -Dproject.version=$TRAVIS_TAG publishSigned ghpagesPushSite
+sbt -jvm-opts travis/jvmopts.compile -Dproject.version=\$TRAVIS_TAG publishSigned ghpagesPushSite
 
